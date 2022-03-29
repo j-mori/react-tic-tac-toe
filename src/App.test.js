7de00c4d2146d6b0f83292c495e33d4e3e78b8c4
Jacopo Mori <jacopo.mori@docebo.com>
2022-03-29 12:10:16 +0200

    Edit react-router from browser router to hash router to better integrate with github pages

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 test('renders learn react link', () => {
-  render(<BrowserRouter><App /></BrowserRouter>);
+  render(<HashRouter><App /></HashRouter>);
   const linkElement = screen.getByText(/Tic tac toe/i);
   expect(linkElement).toBeInTheDocument();
 });
