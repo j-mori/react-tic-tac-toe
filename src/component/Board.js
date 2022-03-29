591b48262e19843952d69b31b6ee7ac4d6d7442e
Jacopo Mori <jacopo.mori@docebo.com>
2022-03-29 16:55:39 +0200

    Removed unused import

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import '../css/Game.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {calculateWinner, renderPlayerSymbol} from "../utils/common";
